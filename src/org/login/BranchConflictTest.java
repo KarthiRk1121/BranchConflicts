80276906869573093417246306d135f8e91c9909
karthiRk1121 <Karthirhappylife@gmail.com>
2023-08-17 11:55:56 +0530

    Developer

--- src/org/login/BranchConflictTest.java
+++ src/org/login/BranchConflictTest.java
@@ -5,7 +5,7 @@ public class BranchConflictTest {
 	public static void main(String[] args) {
 		
 		System.out.println("Branch conflict Learning");
-
+System.out.println("Devloper workspace");
 	}
 
 }
